a4d9d30a7b509924e61171685880caf685c895c6
obaid-majeed <khanaubaid121@gmail.com>
2025-03-18 16:29:37 +0530

    Removed target/ directory from Git tracking

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (username, password, role) VALUES ('admin', 'admin123', 'manager');
-INSERT INTO menu_items (name, price) VALUES ('Pizza', 8.99);
-INSERT INTO orders (user_id, status) VALUES (1, 'pending');
-INSERT INTO order_details (order_id, menu_item_id, quantity) VALUES (1, 1, 3);
